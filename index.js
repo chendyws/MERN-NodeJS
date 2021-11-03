9ae0dbba52cd64e606e0652f1950e7c6b606a8c9
ChendyWS <chendywidyasanti@gmail.com>
2021-11-03 17:43:06 +0800

    basic router, request, & response NodeJS

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 
-app.use(() => {
-    console.log('hello server...')
-    console.log('hello lagi')
+router.use('/products', (req, res, next) => {
+    res.json({name: "Chendy", email: "chendywidyasanti@gmail.com"});
+    next();
 })
+router.use('/price', (req, res, next) => {
+    res.json({price: 300000});
+    next();
+})
+router.get('/customers', (req, res, next) => {
+    res.json({title: "Customer"});
+    next();
+})
+
+app.use('/', router);
+
+//GET '/users' ==> [{name: Chendy}]
 
 app.listen(4000);
