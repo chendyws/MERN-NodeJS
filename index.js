155d28b079206d14853ed0f4c483a3444f6936ff
ChendyWS <chendywidyasanti@gmail.com>
2021-11-12 14:48:17 +0800

    Connection to MonggoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 
 const app = express();
 const authRoutes = require('./src/routes/auth');
@@ -24,6 +25,11 @@ app.use((error, req, res, next) => {
     const data = error.data;
 
     res.status(status).json({message: message, data: data});
-})
+});
 
-app.listen(4000);
+mongoose.connect('mongodb+srv://cws:kj5mxGdR9n5aNETe@cluster0.tquo6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+.then(() => {
+    app.listen(4000, () => console.log('Connection Success'));
+})
+.catch(err => console.log(err));
+
